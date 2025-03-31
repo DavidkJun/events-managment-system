e60436a24881a47ecd2b646e8256f49b96e1806c
Davyd Krykota <krikotadavid957@gmail.com>
2025-03-31 13:21:07 +0300

    updated controlers to be able to see home page

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [UserModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+      serveRoot: '/client'
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
