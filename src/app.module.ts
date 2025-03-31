1512d9351b0d6cafea808ef3c9fd046cf88ea6a3
Davyd Krykota <krikotadavid957@gmail.com>
2025-03-31 22:43:36 +0300

    connected ty MySQL database and TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,14 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {ConfigModule} from '@nestjs/config';
+import databaseConfig from '../config/database.config';
 
 @Module({
   imports: [UserModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(databaseConfig().database),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'client'),
       serveRoot: '/client'
