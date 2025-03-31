610ef62035c0e3746f2ba02e3afc09080d0c1856
Davyd Krykota <krikotadavid957@gmail.com>
2025-03-31 22:42:59 +0300

    added database cofiguration

---
+++ config/database.config.ts
@@ -0,0 +1,14 @@
+import {TypeOrmModuleOptions} from '@nestjs/typeorm';
+
+export default ():  {database: TypeOrmModuleOptions}=> ({
+  database: {
+    type: 'mysql',
+    host: process.env.DB_HOST || 'localhost',
+    port: Number(process.env.DB_PORT) || 3306,
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE || 'test',
+    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+    synchronize: true,
+  }
+});
