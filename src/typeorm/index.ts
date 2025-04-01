a65eb9caa65413f22779e13f91d97c21b416b9a7
Davyd Krykota <krikotadavid957@gmail.com>
2025-04-01 16:21:12 +0300

    added file for exporting enteties

---
+++ src/typeorm/index.ts
@@ -0,0 +1,8 @@
+import {User} from './User';
+import {Admin} from './Admin';
+import {Group} from './Group';
+import {Event} from './Event';
+
+export {User,Admin,Group,Event};
+
+export const entities = [User,Admin,Group,Event]
