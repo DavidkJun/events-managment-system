5c5189af322964049614bed108c44787bf3be211
Davyd Krykota <krikotadavid957@gmail.com>
2025-04-29 11:45:34 +0300

    feat: add user displaying from database

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,16 @@
 import { Controller, Get, Post, Put, Delete, Body } from '@nestjs/common';
 import {UserService} from './user.service';
 import { CreateUserDto } from '../dtos/createUser.dto';
+import { User } from '../typeorm/User';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get()
+  getUsers(): Promise<User[]> {
+    return this.userService.getUsers()
+  }
 
   @Post()
   createUser(@Body() createUserDto: CreateUserDto) {
